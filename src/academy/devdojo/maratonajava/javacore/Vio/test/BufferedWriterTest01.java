d996a6c08909703045b1cb20a9df4a4f437cae62
Anderson <anderson.hericles68@gmail.com>
2023-02-21 20:13:31 -0300

    atualizando meu github

--- src/academy/devdojo/maratonajava/javacore/Vio/test/BufferedWriterTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Vio/test/BufferedWriterTest01.java
@@ -1,20 +1,19 @@
 package academy.devdojo.maratonajava.javacore.Vio.test;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-// File
-// FileWriter
-// FileReader
-//BufferedWriter
-//BufferedReader
-public class FileWriter01 {
+public class BufferedWriterTest01 {
     public static void main(String[] args) {
         File file = new File("file.txt");
-        try(FileWriter fw = new FileWriter(file, true)) {
-            fw.write("O DevDojo é lindo, é o melhor curso do Brasilllllll\nContinuando em outra linha\n");
-            fw.flush();
+        try(FileWriter fw = new FileWriter(file, true);
+        BufferedWriter br = new BufferedWriter(fw)) {
+            br.write("O DevDojo é lindo, é o melhor curso do Brasilllllll");
+            br.newLine();
+            br.flush();
+
 
         } catch (IOException ex) {
             ex.printStackTrace();
