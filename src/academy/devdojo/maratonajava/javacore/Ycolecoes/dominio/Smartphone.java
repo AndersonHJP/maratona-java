a81dfee1c25b59d5c065bcca4994f0564a85e8dc
AndersonHJP <108600014+AndersonHJP@users.noreply.github.com>
2023-03-08 15:44:04 -0300

    aaaaaeeeeee

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/dominio/Smartphone.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/dominio/Smartphone.java
@@ -8,6 +8,7 @@ public class Smartphone {
         this.serialNumber = serialNumber;
         this.marca = marca;
     }
+
     // Reflexivo: x.equals(x) tem quer ser true para tudo que for diferente de null
     // Simetrico: para x e y diferentes de null, se x.equals(y) == true logo , y.equals(x) == true
     // Transitividade: para x,y,z diferentes de null, se x.equals(y) == true, e x.equals(z) == true logo, y.equals(z) == true
@@ -15,7 +16,7 @@ public class Smartphone {
     // para x diferente de null, x.equals(null) tem que retornar false.
     @Override
     public boolean equals(Object obj) {
-        if(obj == null) return false;
+        if (obj == null) return false;
         if (this == obj) return true;
         if (this.getClass() != obj.getClass()) return false;
         Smartphone smartphone = (Smartphone) obj;
@@ -28,7 +29,7 @@ public class Smartphone {
     // y.hashCode() != x.hashCode() x.equals(y) deverá ser false.
     @Override
     public int hashCode() {
-        return serialNumber == null ? 0: this.serialNumber.hashCode();
+        return serialNumber == null ? 0 : this.serialNumber.hashCode();
     }
 
     public String getSerialNumber() {
@@ -40,9 +41,8 @@ public class Smartphone {
     }
 
     public String getMarca() {
-        return marca;
+        return null;
     }
-
     public void setMarca(String marca) {
         this.marca = marca;
     }
